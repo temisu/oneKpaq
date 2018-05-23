8ef30f3ca6aae8d3aa1444d576aa42353b3e2639
ts <tz@iki.fi>
2018-05-23 20:44:50 +0300

    Fixing test

--- testbench/test.sh
+++ testbench/test.sh
@@ -2,6 +2,6 @@
 rm -f test.onekpaq test.raw
 # test results only valid when using the same context cache!
 ../onekpaq_encode 4 3 recede1.data recede2.data recede3.data recede4.data recede5.data test.onekpaq
-diff test.onekpaq recede.onekpaq || exit 1
-../onekpaq_decode recede.onekpaq test.raw
+../onekpaq_decode test.onekpaq test.raw
 diff test.raw recede.raw
+rm -f test.onekpaq test.raw
