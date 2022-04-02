440b77268207018925cfbf460925f9dc06e7b0ae
PoroCYon <porocyon@titandemo.org>
2022-04-03 00:41:45 +0200

    shave off 25 bytes

--- onekpaq_stub_lnx32.asm
+++ onekpaq_stub_lnx32.asm
@@ -43,75 +43,112 @@ bits 32
 org 0x08048000
 
 ehdr:
-	db 0x7F,"ELF"
-	db 1 ; E_CLASS
-	db 1 ; E_DATA
-	db 1 ; E_VERSION
-	db 0 ; E_OSABI
-	times 8 db 0
-	dw 2 ; e_type (EXEC)
-	dw 3 ; e_machine
-	dd 1 ; e_version
-	dd _start ; e_entry
-	dd phdr - ehdr ; e_phoff
-	dd 0 ; e_shoff
-	dd 0 ; e_flags
-	dw ehdr.end-ehdr ; e_ehsize
-	dw phdr.end-phdr ; e_phentsize
-	dw 1 ; e_phnum
-	dw 0 ; e_shentsize
-	dw 0 ; e_shnum
-	dw 0 ; e_shstrndx
-.end:
+	db 0x7F,"ELF" ;!EI_MAGIC
+	db 1 ;~E_CLASS
+	db 1 ;~E_DATA
+
+_endstub1: ; 10 bytes
+	mov eax, SYS_memfd_create ; 5 bytes
+	; ebx: name: don't care
+	xor ecx, ecx ; 2 bytes
+	int 0x80     ; 2 bytes
+
+	db 0x3d ; cmp eax, imm32 -> jump over e_type and e_machine
+
+	;db 1 ; E_VERSION
+	;db 0 ; E_OSABI
+	;times 8 db 0
+	db 2 ;!e_type.0 (EXEC)
+emptystr:
+	db 0 ;!e_type.1 (EXEC)
+	dw 3 ;!e_machine
+
+	;dd 1 ; e_version
+_endstub2: ; 4 bytes
+	push SYS_write ; 2 bytes
+	jmp short _endstub3 ; 2 bytes
+
+	dd _start ;!e_entry
+	dd phdr - ehdr ;!e_phoff
+
+	;dd 0 ; e_shoff
+	;dd 0 ; e_flags
+_endstub3: ; 8 bytes
+	xchg eax, ebx ; 1 byte
+	;pop eax ; 1 byte
+	mov ecx, payload_dest ; 5 bytes
+	jmp short _endstub4 ; 2 bytes
+
+	dw ehdr.end-ehdr ;!e_ehsize
+	dw phdr.end-phdr ;!e_phentsize
+	dw 1 ;!e_phnum
+
+_endstub4: ; >=6 bytes
+	pop eax ; 1 byte
+	mov edx, 0x4000-(payload_dest-ehdr) ; 5 bytes
+ehdr.end:
+	int 0x80 ; 2 bytes
+	jmp short _endstub5
+
+	;dw 0 ; e_shentsize
+	;dw 0 ; e_shnum
+	;dw 0 ; e_shstrndx
 
 phdr:
-	dd 1 ; p_type
-	dd 0 ; p_offset
-	dd ehdr ; p_vaddr
-	dd phdr ; p_paddr
-	dd END-ehdr ; p_filesz
-	dd 0xfc000;END-ehdr ; memsz
-	dd 7 ; p_flags
-	dd 0x1000 ; p_align
-.end:
+	dd 1 ;!p_type
+	dd 0 ;!p_offset
+	dd ehdr ;!p_vaddr
+	dd phdr ;!p_paddr
+	dd END-ehdr ;!p_filesz
+	dd 0xfc000;END-ehdr ;!memsz
+
+	;dd 7 ;~p_flags
+	;dd 0x1000 ; p_align
+	db 7
+_endstub5: ; 7 bytes
+	pop ecx ; 1 byte
+	mov edx, esp ; 2 bytes
+	lea esi, [edx + ecx * 4 + 4] ; 4 bytes
+phdr.end:
+
+	mov ax, SYS_execveat
+	mov ecx, emptystr
+	mov edi, AT_EMPTY_PATH
+	int 0x80
 
 _start:
 	mov ebx, payload+PAYLOAD_OFF
-	;push ebx ; ???
-	;mov edi, payload_dest
 	lea edi, [ebx + payload_dest-payload-PAYLOAD_OFF]
 
 %include "onekpaq_decompressor32.asm"
 
-	mov eax, SYS_memfd_create
-	; ebx: name: whatever
-	xor ecx, ecx
-	int 0x80
+	jmp _endstub1
 
-	xchg eax, ebx
-	push SYS_write
-	pop eax
-	mov ecx, payload_dest
-	mov edx, 0x4000-(payload_dest-ehdr)
-	int 0x80
-	;int3
-
-	pop ecx
-	mov edx, esp ; argv
-	lea esi, [edx + ecx * 4 + 4]
-
-	mov ax, SYS_execveat
-	; ebx: filedes
-	mov ecx, emptystr
-	; edx: argv
-	; esi: envp
-	mov edi, AT_EMPTY_PATH
-	int 0x80
-
-	int3
-
-emptystr:
-	db 0
+;	mov eax, SYS_memfd_create
+;	; ebx: name: whatever
+;	xor ecx, ecx
+;	int 0x80
+;
+;	push SYS_write
+;	xchg eax, ebx
+;	mov ecx, payload_dest
+;	pop eax
+;	mov edx, 0x4000-(payload_dest-ehdr)
+;	int 0x80
+;
+;	pop ecx ; argc
+;	mov edx, esp ; argv
+;	lea esi, [edx + ecx * 4 + 4] ; envp
+;
+;	mov ax, SYS_execveat ; eax was SYS_write result
+;	; ebx: filedes
+;	mov ecx, emptystr
+;	; edx: argv
+;	; esi: envp
+;	mov edi, AT_EMPTY_PATH
+;	int 0x80
+;
+;	int3
 
 payload:
 	;incbin "flag32.raw"
